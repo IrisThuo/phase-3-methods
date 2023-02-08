ad9d873715f8da115783e6dec6295c11b3034d3f
Iris <iris.thuo@student.moringaschool.com>
2023-02-08 03:21:00 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
 # Your code here!
+def greetProgrammer
+  puts "Hello, programmer!"
+end
+
+greetProgrammer
+
+def greet name  
+  puts "Hello, #{name}!"
+end
+
+greet(Iris)
+
+def greetWithDefault (name = "programmer") 
+  puts "Hello, #{name}"
+end
+
+greetWithDefault
+
+def add num1, num2 
+  num1 + num2;
+end
+
+add(1,2)
+
+def halve number 
+  if (typeof number !== "number") 
+    null
+  end 
+
+   number / 2;
+  end
+
+  halve(22)
